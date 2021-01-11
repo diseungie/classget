d835cbe455d26af3278d063ebfd7c549fe62b5e5
diseungie <73057618+diseungie@users.noreply.github.com>
2021-01-12 01:40:36 +0900

    added typetest programme

--- classget/forms.py
+++ classget/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, ValidationError
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, RadioField, ValidationError
 from wtforms.validators import DataRequired, Length, EqualTo
 from classget.models import User
 
@@ -54,3 +54,18 @@ class UpdateAccountForm(FlaskForm):
             user = User.query.filter_by(username=username.data).first()
             if user:
                 raise ValidationError('That username is taken, Please choose a different one')
+
+
+class TypeTestForm(FlaskForm):
+    typetest_choices = [(-30, '1'), (-20, '2'), (-10, '3'), (0, '4'), (3, '5'), (7, '6'), (15, '7')]
+    Q1 = RadioField('Q01.初対面の人と友達になるのが得意だ。', choices=typetest_choices, validators=[DataRequired()])
+    Q2 = RadioField('Q02.人助けをしたり、されたりすることが多い。', choices=typetest_choices, validators=[DataRequired()])
+    Q3 = RadioField('Q03.課題の期限は守るほうだし、遅刻もしない。', choices=typetest_choices, validators=[DataRequired()])
+    Q4 = RadioField('Q04.普段ストレスや不安をなかなか感じないブッダみたいな性格を持っている。', choices=typetest_choices, validators=[DataRequired()])
+    Q5 = RadioField('Q05.芸術や美術は結構好きだ。', choices=typetest_choices, validators=[DataRequired()])
+    Q6 = RadioField('Q06.しばしば新しい冒険についてのアイデアが思い浮かぶ。', choices=typetest_choices, validators=[DataRequired()])
+    Q7 = RadioField('Q07.他の人にどう思われるかなんて気にしない超マイウェイ。', choices=typetest_choices, validators=[DataRequired()])
+    Q8 = RadioField('Q08.人との約束のためなら無理もする。', choices=typetest_choices, validators=[DataRequired()])
+    Q9 = RadioField('Q09.みんなで何かを成し遂げることが好きだ。', choices=typetest_choices, validators=[DataRequired()])
+    Q10 = RadioField('Q10.面白い話で場を盛り上げることができる。', choices=typetest_choices, validators=[DataRequired()])
+    submit = SubmitField('診断結果へ')
--- classget/routes.py
+++ classget/routes.py
@@ -1,8 +1,9 @@
 from flask import render_template, url_for, redirect, request
 from classget import app, db, bcrypt
-from classget.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from classget.forms import RegistrationForm, LoginForm, UpdateAccountForm, TypeTestForm
 from classget.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
+import random
 
 
 @app.route("/")
@@ -48,24 +49,6 @@ def login():
     return render_template('login.html', title='login', form=form, login_error=login_error)
 
 
-@app.route("/logout")
-def logout():
-    logout_user()
-    return redirect(url_for('mainpage'))
-
-
-@app.route("/typetest")
-def typetest():
-    return render_template('typetest.html')
-
-
-@app.route("/mypage")
-@login_required
-def mypage():
-    image_file = url_for('static', filename='img/profile_pics/' + current_user.type + '.jpg')
-    return render_template('mypage.html', title='mypage', image_file=image_file)
-
-
 @app.route("/updateaccount", methods=['GET', 'POST'])
 @login_required
 def updateaccount():
@@ -89,4 +72,50 @@ def updateaccount():
         form.year.data = current_user.year
 
     return render_template('updateaccount.html', title='update account', form=form, password_error=password_error)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('mainpage'))
+
+
+@app.route("/typetest", methods=['GET', 'POST'])
+def typetest():
+    form = TypeTestForm()
+    # すでにキャラ診断した人は結果ページに送る
+    if current_user.type != 'none':
+        return redirect(url_for('typeresult'))
+    if form.validate_on_submit():
+        # formのdataから属性ごとにスコアを集計
+        kaisou_score = int(form.Q1.data) + int(form.Q10.data)
+        penguin_score = int(form.Q2.data) + int(form.Q9.data)
+        iruka_score = int(form.Q3.data) + int(form.Q8.data)
+        kame_score = int(form.Q4.data) + int(form.Q7.data)
+        taco_score = int(form.Q5.data) + int(form.Q6.data)
+        scores = {'kaisou': kaisou_score, 'penguin': penguin_score, 'iruka': iruka_score,
+                  'kame': kame_score, 'taco': taco_score}
+        # 一番高いスコアのキャラを入れる（複数の場合は共同１位のうちランダムで）
+        max_score = max(scores.items(), key=lambda x: x[1])
+        list_max = []
+        for key, value in scores.items():
+            if value == max_score[1]:
+                list_max.append(key)
+        current_user.type = random.choice(list_max)
+        db.session.commit()
+        return redirect(url_for('typeresult'))
+    return render_template('typetest.html', form=form)
+
+
+@app.route("/typeresult", methods=['GET'])
+def typeresult():
+    return render_template('typeresult.html')
+
+
+@app.route("/mypage")
+@login_required
+def mypage():
+    image_file = url_for('static', filename='img/profile_pics/' + current_user.type + '.jpg')
+    return render_template('mypage.html', title='mypage', image_file=image_file)
+
 
